4bf5317f4715492870524d7fd5a19f15f3ba0bc9
kai-ten <kaiherrera76@gmail.com>
2023-05-28 12:41:51 -0400

    Add Digital Signature object

---
+++ digital_signature.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type DigitalSignature struct {
+	CompanyName  string         `json:"company_name" validate:"required"`
+	CreatedTimed *time.Time     `json:"created_time" validate:"omitempty"`
+	DeveloperUID string         `json:"developer_uid" validate:"omitempty"`
+	Fingerprints *[]Fingerprint `json:"fingerprints" validate:"omitempty"`
+	IssuerName   string         `json:"issuer_name" validate:"omitempty"`
+	SerialNumber string         `json:"serial_number" validate:"omitempty"`
+}
+
+func ValidateDigitalSignature(ds *DigitalSignature) (*DigitalSignature, error) {
+	err := validator.New().Struct(ds)
+	if err != nil {
+		ValidatorErrLog(err)
+		return nil, err
+	}
+
+	return ds, nil
+}
---
+++ digital_signature_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestValidDigitalSignatureObject(t *testing.T) {
+	time := time.Now()
+	dg := &DigitalSignature{
+		CompanyName:  "Microsoft Corporation",
+		CreatedTimed: &time,
+		DeveloperUID: "123",
+		Fingerprints: nil,
+		IssuerName:   "LibreSSL",
+		SerialNumber: "345",
+	}
+	_, err := ValidateDigitalSignature(dg)
+	if err != nil {
+		t.Fatalf("Digital Signature should have been valid: %v", err)
+	}
+}
+
+func TestInvalidDigitalSignatureObject(t *testing.T) {
+	time := time.Now()
+	dg := &DigitalSignature{
+		CompanyName:  "",
+		CreatedTimed: &time,
+		DeveloperUID: "123",
+		Fingerprints: nil,
+		IssuerName:   "LibreSSL",
+		SerialNumber: "345",
+	}
+	_, err := ValidateDigitalSignature(dg)
+	if err == nil {
+		t.Fatalf("Digital Signature should have been invalid: %v", err)
+	}
+}
+
+func BenchmarkValidDigitalSignatureObject(b *testing.B) {
+	b.ReportAllocs()
+
+	time := time.Now()
+	dg := &DigitalSignature{
+		CompanyName:  "Microsoft Corporation",
+		CreatedTimed: &time,
+		DeveloperUID: "123",
+		Fingerprints: nil,
+		IssuerName:   "LibreSSL",
+		SerialNumber: "345",
+	}
+
+	ValidateDigitalSignature(dg)
+}
