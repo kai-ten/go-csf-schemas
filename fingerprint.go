f050523151eff3b8952912193acabb7bb61b9aea
kai-ten <kaiherrera76@gmail.com>
2023-05-27 21:27:14 -0400

    Add Fingerprint object

---
+++ fingerprint.go
@@ -0,0 +1,20 @@
+// https://schema.ocsf.io/objects/fingerprint
+package main
+
+import "github.com/go-playground/validator/v10"
+
+type Fingerprint struct {
+	Algorithm   string `json:"algorithm" validate:"omitempty"`
+	AlgorithmID uint8  `json:"algorithm_id" validate:"required"`
+	Value       string `json:"value" validate:"required"`
+}
+
+func ValidateFingerprint(fingerprint *Fingerprint) (*Fingerprint, error) {
+	err := validator.New().Struct(fingerprint)
+	if err != nil {
+		ValidatorErrLog(err)
+		return nil, err
+	}
+
+	return fingerprint, nil
+}
---
+++ fingerprint_test.go
@@ -0,0 +1,51 @@
+package main
+
+import "testing"
+
+func TestValidFingerprintObject(t *testing.T) {
+	fingerprint := &Fingerprint{
+		Algorithm:   "",
+		AlgorithmID: 1,
+		Value:       "ABC123==",
+	}
+	fingerprint, err := ValidateFingerprint(fingerprint)
+	if err != nil {
+		t.Fatalf("Fingerprint object should have been valid: %v", err)
+	}
+
+	t.Logf("Value: %v", fingerprint.Value)
+}
+
+func TestInvalidAlgorithmIDFingerprintObject(t *testing.T) {
+	fingerprint := &Fingerprint{
+		Algorithm: "",
+		Value:     "ABC123==",
+	}
+	_, err := ValidateFingerprint(fingerprint)
+	if err == nil {
+		t.Fatalf("Fingerprint object should have been valid: %v", err)
+	}
+}
+
+func TestInvalidValueFingerprintObject(t *testing.T) {
+	fingerprint := &Fingerprint{
+		Algorithm:   "",
+		AlgorithmID: 1,
+		Value:       "",
+	}
+	_, err := ValidateFingerprint(fingerprint)
+	if err == nil {
+		t.Fatalf("Fingerprint object should have been valid: %v", err)
+	}
+}
+
+func BenchmarkValidFingerprintObject(b *testing.B) {
+	b.ReportAllocs()
+
+	fingerprint := &Fingerprint{
+		Algorithm:   "",
+		AlgorithmID: 1,
+		Value:       "ABC123==",
+	}
+	ValidateFingerprint(fingerprint)
+}
