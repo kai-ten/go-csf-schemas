d0eb65009d06b9219f86bb3158128caf15ee3c32
kai-ten <kaiherrera76@gmail.com>
2023-05-29 20:19:56 -0400

    Add Session object

---
+++ session.go
@@ -0,0 +1,28 @@
+// https://schema.ocsf.io/objects/session
+package main
+
+import (
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type Session struct {
+	CreatedTimed              *time.Time `json:"created_time" validate:"omitempty"`
+	ExpirationTime            *time.Time `json:"expiration_time" validate:"omitempty"`
+	IssuerDetails             string     `json:"issuer" validate:"omitempty"`
+	MultiFactorAuthentication bool       `json:"mfa" validate:"omitempty"`
+	UUID                      string     `json:"uuid" validate:"omitempty"`
+	UniqueID                  string     `json:"uid" validate:"omitempty"`
+	UserCredentialID          string     `json:"credential_uid" validate:"omitempty"`
+}
+
+func ValidateSession(session *Session) (*Session, error) {
+	err := validator.New().Struct(session)
+	if err != nil {
+		ValidatorErrLog(err)
+		return nil, err
+	}
+
+	return session, nil
+}
---
+++ session_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestValidSessionObject(t *testing.T) {
+	ts := time.Now()
+	session := &Session{
+		CreatedTimed:              &ts,
+		ExpirationTime:            &ts,
+		IssuerDetails:             "1",
+		MultiFactorAuthentication: true,
+		UUID:                      "123",
+		UniqueID:                  "234",
+		UserCredentialID:          "456",
+	}
+	_, err := ValidateSession(session)
+	if err != nil {
+		t.Fatalf("Session object should have been valid: %v", err)
+	}
+}
+
+func BenchmarkValidSessionObject(b *testing.B) {
+	b.ReportAllocs()
+
+	ts := time.Now()
+	session := &Session{
+		CreatedTimed:              &ts,
+		ExpirationTime:            &ts,
+		IssuerDetails:             "1",
+		MultiFactorAuthentication: true,
+		UUID:                      "123",
+		UniqueID:                  "234",
+		UserCredentialID:          "456",
+	}
+	ValidateSession(session)
+}
