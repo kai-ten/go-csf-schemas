40c8d92d1edc32352333723dfa03b678571b866e
kai-ten <kaiherrera76@gmail.com>
2023-05-29 21:22:03 -0400

    Add Process object

---
+++ process.go
@@ -0,0 +1,38 @@
+// https://schema.ocsf.io/objects/process
+package main
+
+import (
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type Process struct {
+	CommandLine        string                 `json:"cmd_line" validate:"omitempty"`
+	CreatedTimed       *time.Time             `json:"created_time" validate:"omitempty"`
+	ExtendedAttributes map[string]interface{} `json:"xattributes" validate:"omitempty"`
+	File               *File                  `json:"file" validate:"omitempty"`
+	Integrity          string                 `json:"integrity" validate:"omitempty"`
+	IntegrityLevel     uint8                  `json:"integrity_id" validate:"omitempty"`
+	Lineage            []string               `json:"lineage" validate:"omitempty"`
+	LoadedModules      []string               `json:"loaded_modules" validate:"omitempty"`
+	Name               string                 `json:"name" validate:"omitempty"`
+	ParentProcess      *Process               `json:"parent_process" validate:"omitempty"`
+	ProcessID          uint                   `json:"pid" validate:"omitempty"`
+	ProcessUID         string                 `json:"uid" validate:"omitempty"`
+	Sandbox            string                 `json:"sandbox" validate:"omitempty"`
+	TerminatedTime     *time.Time             `json:"terminated_time" validate:"omitempty"`
+	ThreadID           uint                   `json:"tid" validate:"omitempty"`
+	User               *User                  `json:"user" validate:"omitempty"`
+	UserSession        *Session               `json:"session" validae:"omitempty"`
+}
+
+func ValidateProcess(process *Process) (*Process, error) {
+	err := validator.New().Struct(process)
+	if err != nil {
+		ValidatorErrLog(err)
+		return nil, err
+	}
+
+	return process, nil
+}
---
+++ process_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestValidProcessObject(t *testing.T) {
+	ts := time.Now()
+	process := &Process{
+		CommandLine:    "ssh user@0.0.0.0",
+		CreatedTimed:   &ts,
+		File:           nil,
+		Integrity:      "Windows",
+		IntegrityLevel: 99,
+		Lineage:        []string{"/usr/sbin/sshd"},
+		LoadedModules:  []string{"Docker"},
+		Name:           "Docker",
+		ParentProcess:  nil,
+		ProcessID:      3899,
+		ProcessUID:     "Docker",
+		Sandbox:        "default_ps",
+		TerminatedTime: &ts,
+		ThreadID:       123,
+		User:           nil,
+		UserSession:    nil,
+	}
+
+	_, err := ValidateProcess(process)
+	if err != nil {
+		t.Fatalf("Process object should be valid: %v", err)
+	}
+}
+
+func BenchmarkValidProcessObject(b *testing.B) {
+	b.ReportAllocs()
+
+	ts := time.Now()
+	process := &Process{
+		CommandLine:    "ssh user@0.0.0.0",
+		CreatedTimed:   &ts,
+		File:           nil,
+		Integrity:      "Windows",
+		IntegrityLevel: 99,
+		Lineage:        []string{"/usr/sbin/sshd"},
+		LoadedModules:  []string{"Docker"},
+		Name:           "Docker",
+		ParentProcess:  nil,
+		ProcessID:      3899,
+		ProcessUID:     "Docker",
+		Sandbox:        "default_ps",
+		TerminatedTime: &ts,
+		ThreadID:       123,
+		User:           nil,
+		UserSession:    nil,
+	}
+
+	ValidateProcess(process)
+}
