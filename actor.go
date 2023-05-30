2ab3964c724226d143641f5fdf4d69b3a96703ef
kai-ten <kaiherrera76@gmail.com>
2023-05-29 21:34:03 -0400

    Add Actor object

---
+++ actor.go
@@ -0,0 +1,23 @@
+// https://schema.ocsf.io/objects/actor
+package main
+
+import "github.com/go-playground/validator/v10"
+
+type Actor struct {
+	AuthorizationInformation *[]AuthorizationInformation `json:"authorizations" validate:"omitempty"`
+	IdentityProvider         *IdentityProvider           `json:"idp" validate:"omitempty"`
+	InvokedBy                string                      `json:"invoked_by" validate:"omitempty"`
+	Process                  *Process                    `json:"process" validate:"omitempty"`
+	User                     *User                       `json:"user" validate:"omitempty"`
+	UserSession              *Session                    `json:"session" validate:"omitempty"`
+}
+
+func ValidateActor(actor *Actor) (*Actor, error) {
+	err := validator.New().Struct(actor)
+	if err != nil {
+		ValidatorErrLog(err)
+		return nil, err
+	}
+
+	return actor, nil
+}
---
+++ actor_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+func TestValidActorObject(t *testing.T) {
+	actor := &Actor{
+		AuthorizationInformation: &[]AuthorizationInformation{},
+		IdentityProvider:         &IdentityProvider{},
+		InvokedBy:                "",
+		Process:                  &Process{},
+		User:                     &User{EmailAddress: "test@email.com"},
+		UserSession:              &Session{},
+	}
+	_, err := ValidateActor(actor)
+	if err != nil {
+		t.Fatalf("Actor object should have been valid: %v", err)
+	}
+}
+
+func BenchmarkValidActorObject(b *testing.B) {
+	b.ReportAllocs()
+
+	actor := &Actor{
+		AuthorizationInformation: &[]AuthorizationInformation{},
+		IdentityProvider:         &IdentityProvider{},
+		InvokedBy:                "",
+		Process:                  &Process{},
+		User:                     &User{},
+		UserSession:              &Session{},
+	}
+	ValidateActor(actor)
+}
